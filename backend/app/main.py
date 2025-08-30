87c921561686059a03fc42c9711939783e3d2d5b
Kaua <kaua.ferreiracavalcante@gmail.com>
2025-08-30 00:38:23 -0300

    add CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from .classifier import classify_email
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173/",
+    "https://clear-box.vercel.app/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 class EmailRequest(BaseModel):
     emails: list[str]
